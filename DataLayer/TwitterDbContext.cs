dbf8b505c9c706663016e73e8df8268a20ba5128
Amirreza <83154350+AMIR34A@users.noreply.github.com>
2022-04-22 11:33:46 +0430

    Add files via upload

---
+++ DataLayer/TwitterDbContext.cs
@@ -0,0 +1,10 @@
+﻿using DataLayer.Models;
+using System.Data.Entity;
+namespace DataLayer
+{
+    public class TwitterDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Information> Informations { get; set; }
+    }
+}
